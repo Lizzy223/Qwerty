0b7aa862ff533f3f84a7bf0e1ac04f080538892f
Akinyemi Oluwabukunmi <braingrams40@gmail.com>
2022-05-19 12:43:42 +0100

    Fixed search

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,143 +1,155 @@
 import {
-    Box,
-    Center,
-    Text,
-    Stack,
-    List,
-    Flex,
-    Spacer,
-    Heading,
-    useColorModeValue,
-  } from '@chakra-ui/react';
+	Box,
+	Center,
+	Text,
+	Stack,
+	List,
+	Flex,
+	Spacer,
+	Heading,
+	useColorModeValue,
+} from "@chakra-ui/react";
 
-  const JobContainer = (props) =>{
-      const {title, location, description, price} = props;
-      return(
-    <Center py={6} m={'2rem'} >
-        <Box
-          maxW={'330px'}
-          w={'full'}
-          bg={useColorModeValue('white', 'gray.800')}
-          boxShadow={'2xl'}
-          rounded={'md'}
-          overflow={'hidden'}>
-          <Stack
-            textAlign={'center'}
-            p={6}
-            color={useColorModeValue('gray.800', 'white')}
-            align={'center'}>
-            <Text
-              fontSize={'sm'}
-              fontWeight={500}
-              bg={useColorModeValue('green.50', 'green.900')}
-              p={2}
-              px={3}
-              color={'green.500'}
-              rounded={'full'}>
-              {title}
-            </Text>
-            <Stack direction={'row'} align={'center'} justify={'center'}>              
-              <Text fontSize={'3xl'} fontWeight={800}>
-                {price}
-              </Text>
-              <Text color={'gray.500'}>/month</Text>
-            </Stack>
-          </Stack>
-  
-          <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10}>
-            <List spacing={3}>
-              <Text>{description}</Text>              
-            </List>
-            <Text>{location}</Text>
-          </Box>
-        </Box>
-        <Spacer/>
-      </Center>
-       );
-  } 
-  
+export const JobContainer = (props) => {
+	const { title, location, description, price } = props;
+	return (
+		<Center py={6} m={"2rem"}>
+			<Box
+				maxW={"330px"}
+				w={"full"}
+				bg={useColorModeValue("white", "gray.800")}
+				boxShadow={"2xl"}
+				rounded={"md"}
+				overflow={"hidden"}
+			>
+				<Stack
+					textAlign={"center"}
+					p={6}
+					color={useColorModeValue("gray.800", "white")}
+					align={"center"}
+				>
+					<Text
+						fontSize={"sm"}
+						fontWeight={500}
+						bg={useColorModeValue("green.50", "green.900")}
+						p={2}
+						px={3}
+						color={"green.500"}
+						rounded={"full"}
+					>
+						{title}
+					</Text>
+					<Stack direction={"row"} align={"center"} justify={"center"}>
+						<Text fontSize={"3xl"} fontWeight={800}>
+							{price}
+						</Text>
+						<Text color={"gray.500"}>/month</Text>
+					</Stack>
+				</Stack>
 
-  export const JobList = [
-      {
-          JobId:"1",
-          JobTitle: 'Dish Washer',
-          JobLocation: 'Alagbado',
-          JobDescription: 'Wash plates every evening ',
-          JobPrice:'N30,000'
-      },
-      {
-        JobId:"2",
-        JobTitle: 'Accountant',
-        JobLocation: 'MainLand',
-        JobDescription: 'Ensures all accounts are balances ',
-        JobPrice:'N100,000'
-    },
-    {   
-        JobId:"3",
-        JobTitle: 'HR',
-        JobLocation: 'Lekki',
-        JobDescription: 'Oversee all employers',
-        JobPrice:'N80,000'
-    },
-    {
-        JobId:"4",
-        JobTitle: 'Data Analyst',
-        JobLocation: 'Surulere',
-        JobDescription: 'Collect, analyse data from customers',
-        JobPrice:'N100,000'
-    },
-    {
-        JobId:"5",
-        JobTitle: 'Dish Washer',
-        JobLocation: 'Alagbado',
-        JobDescription: 'Wash plates every evening ',
-        JobPrice:'N30,000'
-    },
-    {
-      JobId:"6",
-      JobTitle: 'Accountant',
-      JobLocation: 'MainLand',
-      JobDescription: 'Ensures all accounts are balances ',
-      JobPrice:'N100,000'
-  },
-  {   
-      JobId:"7",
-      JobTitle: 'HR',
-      JobLocation: 'Lekki',
-      JobDescription: 'Oversee all employers',
-      JobPrice:'N80,000'
-  },
-  {
-      JobId:"8",
-      JobTitle: 'Data Analyst',
-      JobLocation: 'Surulere',
-      JobDescription: 'Collect, analyse data from customers',
-      JobPrice:'N100,000'
-  },
-    ]
+				<Box bg={useColorModeValue("gray.50", "gray.900")} px={6} py={10}>
+					<List spacing={3}>
+						<Text>{description}</Text>
+					</List>
+					<Text>{location}</Text>
+				</Box>
+			</Box>
+			<Spacer />
+		</Center>
+	);
+};
 
-  export  function JobLists() {
-    return (
-      <Box
-      
-      minH={'50vh'}  
-      alignItems={'center'}
-      justifyContent={'center'}
-      p={5} m={"2rem"}>
-          <Center>
-            <Heading color={'gray.500'} p={['1.25rem', '2.5rem']}>Job Updates</Heading>
-          </Center>
-          <Stack >
-              <Flex flexWrap={'wrap'}>
-              {
-                 JobList.map(({JobId, JobTitle, JobLocation, JobDescription, JobPrice}) =>(
-                     <JobContainer key={JobId} title={JobTitle} location={JobLocation} description={JobDescription} price={JobPrice} />
-                 )
+export const JobList = [
+	{
+		JobId: "1",
+		JobTitle: "Dish Washer",
+		JobLocation: "Alagbado",
+		JobDescription: "Wash plates every evening ",
+		JobPrice: "N30,000",
+	},
+	{
+		JobId: "2",
+		JobTitle: "Accountant",
+		JobLocation: "MainLand",
+		JobDescription: "Ensures all accounts are balances ",
+		JobPrice: "N100,000",
+	},
+	{
+		JobId: "3",
+		JobTitle: "HR",
+		JobLocation: "Lekki",
+		JobDescription: "Oversee all employers",
+		JobPrice: "N80,000",
+	},
+	{
+		JobId: "4",
+		JobTitle: "Data Analyst",
+		JobLocation: "Surulere",
+		JobDescription: "Collect, analyse data from customers",
+		JobPrice: "N100,000",
+	},
+	{
+		JobId: "5",
+		JobTitle: "Dish Washer",
+		JobLocation: "Alagbado",
+		JobDescription: "Wash plates every evening ",
+		JobPrice: "N30,000",
+	},
+	{
+		JobId: "6",
+		JobTitle: "Accountant",
+		JobLocation: "MainLand",
+		JobDescription: "Ensures all accounts are balances ",
+		JobPrice: "N100,000",
+	},
+	{
+		JobId: "7",
+		JobTitle: "HR",
+		JobLocation: "Lekki",
+		JobDescription: "Oversee all employers",
+		JobPrice: "N80,000",
+	},
+	{
+		JobId: "8",
+		JobTitle: "Data Analyst",
+		JobLocation: "Surulere",
+		JobDescription: "Collect, analyse data from customers",
+		JobPrice: "N100,000",
+	},
+];
 
-                 ) 
-  }
-            </Flex>
-          </Stack>
-      </Box>
-    );
-  }
+export const JobsBox = () => {
+	return (
+		<Stack>
+			<Flex flexWrap={"wrap"}>
+				{JobList.map((job) => (
+					<JobContainer
+						key={job.JobId}
+						title={job.JobTitle}
+						location={job.JobLocation}
+						description={job.JobDescription}
+						price={job.JobPrice}
+					/>
+				))}
+			</Flex>
+		</Stack>
+	);
+};
+export function JobLists() {
+	return (
+		<Box
+			minH={"50vh"}
+			alignItems={"center"}
+			justifyContent={"center"}
+			p={5}
+			m={"2rem"}
+		>
+			<Center>
+				<Heading color={"gray.500"} p={["1.25rem", "2.5rem"]}>
+					Job Updates
+				</Heading>
+			</Center>
+			<JobsBox />
+		</Box>
+	);
+}
--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,14 +1,15 @@
-import { React} from "react";
+import { React, useState } from "react";
 
 import Footer from "./../components/Footer";
-import {  JobLists } from "./../components/JobList";
+import { JobLists, JobList, JobContainer } from "./../components/JobList";
 import {
-  Stack,
-  InputGroup,
-  InputLeftElement,
-  Input,
-  Flex,
-  Button,
+	Stack,
+	InputGroup,
+	InputLeftElement,
+	Input,
+	Flex,
+	Button,
+	Box,
 } from "@chakra-ui/react";
 // import icon1 from '../assets/images/icon1.svg'
 // import icon2 from '../assets/images/icon2.svg'
@@ -16,57 +17,96 @@ import {
 import { GoLocation, GoSearch } from "react-icons/go";
 
 const Jobs = () => {
-  //const [search, setSearch] = useState("");
-  return (
-    <div>
-      <Stack direction={["column", "row"]} mt={"3rem"} p={"1.5rem"}>
-        <Flex flex={1} align={"center"} justify={"center"}>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none" children={GoSearch} />
-            <Input color="#ccc" placeholder="search" />
-          </InputGroup>
-        </Flex>
-        <Flex flex={1} align={"center"} justify={"center"}>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none" children={GoLocation} />
-            <Input
-              placeholder="state"
-              color="#ccc"
-            />
-          </InputGroup>
-        </Flex>
-        <Flex flex={1} align={"center"} justify={"center"}>
-          {/* <InputGroup>
-              <InputLeftElement
-                pointerEvents='none'
-                
-                children={GoSearch}
-              />
-              <Input placeholder='Enter amount' />
-            </InputGroup> */}
-          <Button p={"1rem"} color={"#fff"} bg={"green"} cursor={"pointer"}>
-            Search
-          </Button>
-        </Flex>
-      </Stack>
-      {/* {
-          JobList.filter((JobTitle) => {
-            if (search === ""){
-              return JobTitle
-            } else if (JobTitle.name.toLowerCase().includes(search.toLocaleLowerCase())){
-              return JobTitle
-            }
-          }).map((JobTitle, key) =>{
-            return(
-              <JobLists {...JobTitle.JobList}/>
-            );
-          })
-        } */}
+	const [searchData, setSearchData] = useState([]);
+	const [title, setTitle] = useState("");
 
-      <JobLists />
-      <Footer />
-    </div>
-  );
+	const searchJobs = () => {
+		const searchWord = {
+			title: title,
+		};
+		const newSearch = JobList.filter((value) => {
+			return value.JobTitle.toLowerCase().includes(
+				searchWord.title.toLowerCase()
+			);
+		});
+		if (searchWord.title === "") {
+			setSearchData([]);
+		} else {
+			setSearchData(newSearch);
+		}
+		console.log(searchWord);
+	};
+	return (
+		<div>
+			<Stack direction={["column", "row"]} mt={"3rem"} p={"1.5rem"}>
+				<Flex flex={1} align={"center"} justify={"center"}>
+					<InputGroup>
+						<InputLeftElement pointerEvents="none" children={GoSearch} />
+						<Input
+							color="#ccc"
+							placeholder="search"
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+					</InputGroup>
+				</Flex>
+				<Flex flex={1} align={"center"} justify={"center"}>
+					<InputGroup>
+						<InputLeftElement pointerEvents="none" children={GoLocation} />
+						<Input placeholder="state" color="#ccc" />
+					</InputGroup>
+				</Flex>
+				<Flex flex={1} align={"center"} justify={"center"}>
+					<InputGroup>
+						<InputLeftElement pointerEvents="none" children={GoSearch} />
+						<Input placeholder="Enter amount" />
+					</InputGroup>
+					<Button
+						p={"1rem"}
+						color={"#fff"}
+						bg={"green"}
+						cursor={"pointer"}
+						onClick={() => searchJobs()}
+					>
+						Search
+					</Button>
+				</Flex>
+			</Stack>
+			{searchData.length !== 0 && (
+				<Box>
+					{searchData.slice(0, 3).map((job, key) => {
+						return (
+							<Stack>
+								<Flex flexWrap={"wrap"}>
+									<JobContainer
+										key={job.JobId}
+										title={job.JobTitle}
+										location={job.JobLocation}
+										description={job.JobDescription}
+										price={job.JobPrice}
+									/>
+								</Flex>
+							</Stack>
+						);
+					})}
+				</Box>
+			)}
+			{/* {JobList.filter((JobTitle) => {
+				if (search === "") {
+					return JobTitle;
+				} else if (
+					JobTitle.name.toLowerCase().includes(search.toLocaleLowerCase())
+				) {
+					return JobTitle;
+				}
+			}).map((JobTitle, key) => {
+				return <JobLists {...JobTitle.JobList} />;
+			})} */}
+
+			<JobLists />
+			<Footer />
+		</div>
+	);
 };
 
 export default Jobs;
